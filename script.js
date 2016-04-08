796f41c737968ce444ed56663095aa6af4167475
Syrexide <wwc.302@gmail.com>
2016-04-08 16:58:32 -0400

    site

---
+++ script.js
@@ -0,0 +1,132 @@
+// bg color
+var colors = ["#1abf9f", "#2ecc71", "#3498db", "#9b59b6", "#e67e22", "#e74c3c"];
+var color = colors[Math.floor(Math.random() * colors.length)];
+$("body").css("background", color);
+// particlesjs
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 5
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": false,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 3,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "top",
+            "random": true,
+            "straight": true,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "bubble"
+            },
+            "onclick": {
+                "enable": false,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 1
+                }
+            },
+            "bubble": {
+                "distance": 200,
+                "size": 20,
+                "duration": 2,
+                "opacity": 0,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+// nprogress (this does nothing i just like nprogress lol)
+NProgress.configure({
+    showSpinner: false
+});
+NProgress.start();
+$(window).on("load", function() {
+    NProgress.done()
+});
+$("h1").on("click", function() {
+    $(this).css({
+        "font-size": "1000%",
+        "top": "49%",
+        "text-shadow": "6px 6px 5px rgba(0,0,0,0.1),0 0 5px #fff"
+    });
+    setTimeout(300);
+    window.location.href = "https://github.com/wc1202";
+});
