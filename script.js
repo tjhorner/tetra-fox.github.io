db244c584fa1a20bbd94de0aacf4004e708dbe4a
Syrexide <wwc.302@gmail.com>
2016-04-13 20:29:30 -0400

    site

--- script.js
+++ script.js
@@ -131,6 +131,6 @@ $("h1").on("click", function() {
     window.location.href = "https://github.com/wc1202";
 });
 // force https
-if (window.location.protocol != "https:") {
+if (window.location.protocol !== "https:") {
     window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
 }
