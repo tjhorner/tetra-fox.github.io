476adcb962688a5eaab8c5b7c997ac1e1d8884b6
wc1202 <wwc.302@gmail.com>
2017-03-01 16:46:22 -0500

    more 'optimization'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,91 +1,89 @@
 $(window).on("focus", function() {
-    document.title = "tetra is fluffy~"
+    document.title = "tetra is fluffy~";
 });
 $(window).on("blur", function() {
-    document.title = "come back! <3"
+    document.title = "come back! <3";
 });
 
 $("input[name='linksBtn']").click(function() {
-    if (!isElementInViewport($("#links"))) {
+    if (!isElementInViewport($("#icons"))) {
         /*$("html, body").animate({
             scrollTop: $(document).height()
-        }, 1000, linkAni())*/
-        $("#pullover").removeClass("hidden");
-        $("#pullover").addClass("show");
-        $("#wrapper").removeClass("show");
-        $("#wrapper").addClass("hidden");
+        }, 1000, linkAni())
+        setTimeout(function() {
+            $("#icons").removeClass("tada");
+        }, 1100);*/
+        $("#links, #wrapper").toggleClass("show");
+        setTimeout(function() {
+            $("#links").css("overflow", "visible");
+        }, 500);
     } else {
         linkAni();
     }
 });
 $("input[name='closeBtn']").click(function() {
-        $("#pullover").removeClass("show");
-        $("#pullover").addClass("hidden");
-        $("#wrapper").removeClass("hidden");
-        $("#wrapper").addClass("show");
+    $("#links, #wrapper").toggleClass("show");
+    $("#links").css("overflow", "");
 });
 
 function linkAni() {
-    $("#links").addClass("tada");
+    $("#icons").addClass("tada");
     setTimeout(function() {
-        $("#links").removeClass("tada")
+        $("#icons").removeClass("tada");
     }, 1100);
-    $(".fa-github").addClass("hover");
+    $("#icons .fa-github").addClass("hover");
     setTimeout(function() {
-        $(".fa-github").removeClass("hover");
-        $(".fa-key").addClass("hover")
+        $("#icons .fa-github").removeClass("hover");
+        $("#icons .fa-key").addClass("hover");
     }, 100);
     setTimeout(function() {
-        $(".fa-key").removeClass("hover");
-        $(".fa-telegram").addClass("hover")
+        $("#icons .fa-key").removeClass("hover");
+        $("#icons .fa-telegram").addClass("hover");
     }, 200);
     setTimeout(function() {
-        $(".fa-telegram").removeClass("hover");
-        $(".fa-twitter").addClass("hover")
+        $("#icons .fa-telegram").removeClass("hover");
+        $("#icons .fa-twitter").addClass("hover");
     }, 300);
     setTimeout(function() {
-        $(".fa-twitter").removeClass("hover");
-        $(".fa-reddit-alien").addClass("hover")
-    }, 400);;
+        $("#icons .fa-twitter").removeClass("hover");
+        $("#icons .fa-reddit-alien").addClass("hover");
+    }, 400);
     setTimeout(function() {
-        $(".fa-reddit-alien").removeClass("hover");
-        $(".fa-steam").addClass("hover")
+        $("#icons .fa-reddit-alien").removeClass("hover");
+        $("#icons .fa-steam").addClass("hover");
     }, 500);
     setTimeout(function() {
-        $(".fa-steam").removeClass("hover");
-        $(".fa-medium").addClass("hover")
+        $("#icons .fa-steam").removeClass("hover");
+        $("#icons .fa-medium").addClass("hover");
     }, 600);
     setTimeout(function() {
-        $(".fa-medium").removeClass("hover");
-        $(".fa-tumblr").addClass("hover")
+        $("#icons .fa-medium").removeClass("hover");
+        $("#icons .fa-tumblr").addClass("hover");
     }, 700);
     setTimeout(function() {
-        $(".fa-tumblr").removeClass("hover");
-        $(".fa-soundcloud").addClass("hover")
+        $("#icons .fa-tumblr").removeClass("hover");
+        $("#icons .fa-soundcloud").addClass("hover");
     }, 800);
     setTimeout(function() {
-        $(".fa-soundcloud").removeClass("hover");
-        $(".fa-youtube-play").addClass("hover")
+        $("#icons .fa-soundcloud").removeClass("hover");
+        $("#icons .fa-youtube-play").addClass("hover");
     }, 900);
     setTimeout(function() {
-        $(".fa-youtube-play").removeClass("hover");
-        $(".fa-twitch").addClass("hover")
+        $("#icons .fa-youtube-play").removeClass("hover");
+        $("#icons .fa-twitch").addClass("hover");
     }, 1000);
     setTimeout(function() {
-        $(".fa-twitch").removeClass("hover")
+        $("#icons .fa-twitch").removeClass("hover");
     }, 1100);
 }
 
 // http://stackoverflow.com/a/7557433/2621063
 function isElementInViewport(el) {
-
     //special bonus for those using jQuery
-    if (typeof jQuery === "function" && el instanceof jQuery) {
+    if (typeof $ === "function" && el instanceof $) {
         el = el[0];
     }
-
     var rect = el.getBoundingClientRect();
-
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
